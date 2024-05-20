b7d2577661ee1e4533f8d30d6e48dcb4677b2bde
oreste-abizera <oresteabizera11@gmail.com>
2024-05-20 21:51:56 +0200

    refactor: improve layout for small mobile devices with BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -67,6 +67,8 @@ class _MyHomePageState extends State<MyHomePage> {
   var selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
+    var colorScheme = Theme.of(context).colorScheme;
+
     Widget page;
     switch (selectedIndex) {
       case 0:
@@ -79,41 +81,79 @@ class _MyHomePageState extends State<MyHomePage> {
         throw UnimplementedError('no widget for $selectedIndex');
     }
 
-    return LayoutBuilder(builder: (context, constraints) {
-      return Scaffold(
-        body: Row(
-          children: [
-            SafeArea(
-              child: NavigationRail(
-                extended: constraints.maxWidth > 600,
-                destinations: [
-                  NavigationRailDestination(
+    // The container for the current page, with its background color
+    // and subtle switching animation.
+    var mainArea = ColoredBox(
+      color: colorScheme.primaryContainer,
+      child: AnimatedSwitcher(
+        duration: Duration(milliseconds: 200),
+        child: SafeArea(child: page),
+      ),
+    );
+
+    return Scaffold(
+      body: LayoutBuilder(builder: (context, constraints) {
+        if (constraints.maxWidth < 450) {
+          // Use a more mobile-friendly layout with BottomNavigationBar
+          // on narrow screens.
+          return Column(
+            children: [
+              Expanded(child: mainArea),
+              BottomNavigationBar(
+                items: [
+                  BottomNavigationBarItem(
                     icon: Icon(Icons.home),
-                    label: Text('Home'),
+                    label: 'Home',
                   ),
-                  NavigationRailDestination(
+                  BottomNavigationBarItem(
                     icon: Icon(Icons.favorite),
-                    label: Text('Favorites'),
+                    label: 'Favorites',
                   ),
                 ],
-                selectedIndex: selectedIndex,
-                onDestinationSelected: (value) {
+                currentIndex: selectedIndex,
+                onTap: (value) {
                   setState(() {
                     selectedIndex = value;
                   });
                 },
               ),
-            ),
-            Expanded(
-              child: Container(
-                color: Theme.of(context).colorScheme.primaryContainer,
-                child: page,
+            ],
+          );
+        } else {
+          return Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: constraints.maxWidth > 600,
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Home'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.favorite),
+                      label: Text('Favorites'),
+                    ),
+                  ],
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (value) {
+                    setState(() {
+                      selectedIndex = value;
+                    });
+                  },
+                ),
               ),
-            ),
-          ],
-        ),
-      );
-    });
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: mainArea,
+                ),
+              ),
+            ],
+          );
+        }
+      }),
+    );
   }
 }
 
@@ -160,7 +200,7 @@ class GeneratorPage extends StatelessWidget {
               ),
             ],
           ),
-          Spacer(flex: 2),
+          Spacer(flex: 1),
         ],
       ),
     );
